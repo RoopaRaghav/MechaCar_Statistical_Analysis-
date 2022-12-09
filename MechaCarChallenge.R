d886555ad2bf44b239c4e1105704e39a9b4a682d
RoopaRaghav <111100908+RoopaRaghav@users.noreply.github.com>
2022-12-08 19:19:01 -0800

    Updated the script with deliverables.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -3,7 +3,7 @@
 #Use the library() function to load the dplyr package.
 library("dplyr")
 
-#Import and read in the MechaCar_mpg.csv file as a dataframe.
+#Import and read in the MechaCar_mpg.csv file as a data frame.
 mechaCar <- read.csv(file="MechaCar_mpg.csv")
 head(mechaCar)
 
@@ -28,7 +28,7 @@ suspensionCoil <- read.csv(file="Suspension_Coil.csv")
 head(suspensionCoil)
 
 
-#Write an RScript that creates a total_summary dataframe using the summarize()
+#Write an R Script that creates a total_summary data frame using the summarize()
 #function to get the mean, median, variance, and standard deviation of the
 #suspension coil’s PSI column.
 summary(suspensionCoil)
@@ -40,3 +40,16 @@ totalSummary
 lotSummary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarise(mean = mean(PSI),median = median(PSI),
                                              sd = sd(PSI),var = var(PSI) , n=n())
 lotSummary
+
+
+# Part 3: T-Tests on Suspension Coils
+#t-test to determine consistency in sample means
+ttestWhole <- t.test(suspensionCoil$PSI, mu=1500)
+ttestWhole
+#t-tests for individual lots
+ttestLot1 <- t.test(subset(suspensionCoil, Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+ttestLot1
+ttestLot2 <- t.test(subset(suspensionCoil, Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+ttestLot2
+ttestLot3 <- t.test(subset(suspensionCoil, Manufacturing_Lot=="Lot3")$PSI, mu=1500)
+ttestLot3
